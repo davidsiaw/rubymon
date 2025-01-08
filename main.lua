f20a4b32a5e323f669973d6561230b3ef5d29e17
David Siaw <874280+davidsiaw@users.noreply.github.com>
2025-01-08 23:02:44 +0900

    list peripherals

--- main.lua
+++ main.lua
@@ -1,10 +1,18 @@
+local peripheral_list = peripheral.getNames();
+
 local monitor = peripheral.wrap("monitor_0");
 
 monitor.clear();
 monitor.setTextScale(0.5);
 monitor.setCursorPos(1,1);
 monitor.setPaletteColor(colors.white, 0xeeeeee);
-monitor.write("Starting up.")
+monitor.write("Starting up\n")
+
+monitor.write("Found peripherals\n");
+for name in peripheral_list do
+  local typ = getType(name);
+  monitor.write(""..name.." ("..typ..")");
+end
 
 while true do
   local event, side, x, y = os.pullEvent("monitor_touch");
